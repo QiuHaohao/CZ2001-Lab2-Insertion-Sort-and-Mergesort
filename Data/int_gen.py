3bb0041f255d3fb45e556e37e0f6f4106fcb99da
Qiu Haoze <qiuh0008@e.ntu.edu.sg>
2017-10-17 20:04:47 +0800

    Reduced the data size
    from 10000-100000 to 1000 - 9000

--- Data/int_gen.py
+++ Data/int_gen.py
@@ -1,7 +1,8 @@
 from random import randint,choice
 import sys
 
-for size in range(100000,1000001,100000):
+
+for size in range(1000,10000,1000):
 	with open(str(size)+".txt","w") as f:
 		for x in range(size):
 			f.write(str(randint(0,2147483648))+"\n")
