2506ef0a1739c66b9c2bbb1adfa8502373c2eeef
Qiu Haoze <qiuh0008@e.ntu.edu.sg>
2017-10-27 23:30:46 +0800

    Added different datasets
    Added dataset selection in Main.java, and 3 different
    datas(random/descending/ascending) under Data folder

--- Data/int_gen.py
+++ Data/int_gen.py
@@ -3,8 +3,11 @@ import sys
 
 
 for size in range(1000,10000,1000):
+	array = [randint(0,2147483648) for x in range(size)]
+	array.sort()
 	with open(str(size)+".txt","w") as f:
 		for x in range(size):
-			f.write(str(randint(0,2147483648))+"\n")
+			f.write(str(array[-x-1])+"\n")
+			
 
 
--- Main.java
+++ Main.java
@@ -3,59 +3,86 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Main {
 	public static void main(String[] args) throws FileNotFoundException,IOException{
-	File folder = new File("Data/");
-	File[] listOfFiles = folder.listFiles();
-	ArrayList<File> textFileList = new ArrayList<File>();
-	PrintWriter resultOut = new PrintWriter(new BufferedWriter(new FileWriter("result.csv")));
-	//Scan the list of .txt files in /Data/
-    for (int i = 0; i < listOfFiles.length; i++) {
-    	if (listOfFiles[i].isFile()) {
-    		if(listOfFiles[i].getName().endsWith(".txt"))
-        		textFileList.add(listOfFiles[i]);
-      		} 
-    	}
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose dataset:");
+        System.out.println("1. Random");
+        System.out.println("2. Descending");
+        System.out.println("3. Ascending");
+        int choice = sc.nextInt();
+        String dataset;
+        switch(choice){
+            case 1:
+                dataset = "Random";
+                System.out.println("Using random dataset.");
+                break;
+            case 2:
+                dataset = "Descending";
+                System.out.println("Using descending dataset.");
+                break;
+            case 3:
+                dataset = "Ascending";
+                System.out.println("Using ascending dataset.");
+                break;
+            default:
+                dataset = "Random";
+                System.out.println("Wrong input, using random.");
+        }
+    	File folder = new File("Data/"+dataset+"/");
+    	File[] listOfFiles = folder.listFiles();
+    	ArrayList<File> textFileList = new ArrayList<File>();
+    	PrintWriter resultOut = new PrintWriter(new BufferedWriter(new FileWriter("result_"+dataset+".csv")));
+    	//Scan the list of .txt files in /Data/
+        for (int i = 0; i < listOfFiles.length; i++) {
+        	if (listOfFiles[i].isFile()) {
+        		if(listOfFiles[i].getName().endsWith(".txt"))
+            		textFileList.add(listOfFiles[i]);
+          		} 
+        	}
+        
+        long sortingTime;
+    	long startTime;
+    	int numOfKeyComparisms;
+        resultOut.print("Dataset: " + dataset+"\n");
+    	resultOut.print("Insertion Sort\n");
+    	resultOut.print("Data Size,	CPU Time,	Number of Key Comparism\n");
+        for(File f:textFileList){
+        	Sorter s = new Sorter();
+        	Scanner fi = new Scanner(f);
+        	String fileName = f.getName();
+        	int dataSize = Integer.parseInt(fileName.substring(0,fileName.length()-4));
+     		int[] arr = new int[dataSize];
+     		//read in data
+     		for (int i=0;i<dataSize;i++)
+    			arr[i] = Integer.parseInt(fi.next());
 
-    long sortingTime;
-	long startTime;
-	int numOfKeyComparisms;
-	resultOut.print("Insertion Sort\n");
-	resultOut.print("Data Size,	CPU Time,	Number of Key Comparism\n");
-    for(File f:textFileList){
-    	Sorter s = new Sorter();
-    	Scanner fi = new Scanner(f);
-    	String fileName = f.getName();
-    	int dataSize = Integer.parseInt(fileName.substring(0,fileName.length()-4));
- 		int[] arr = new int[dataSize];
- 		//read in data
- 		for (int i=0;i<dataSize;i++)
-			arr[i] = Integer.parseInt(fi.next());
+     		startTime = System.nanoTime();
+     		s.insertionSort(arr,dataSize);
+     		sortingTime = System.nanoTime() - startTime;
 
- 		startTime = System.nanoTime();
- 		s.insertionSort(arr,dataSize);
- 		sortingTime = System.nanoTime() - startTime;
+     		numOfKeyComparisms = s.getInsertionSortKeyComp();
+     		resultOut.print(dataSize+",	"+sortingTime+",	"+numOfKeyComparisms+"\n");
+        	}
+        resultOut.print("MergeSort\n");
+        resultOut.print("Data Size,	CPU Time,	Number of Key Comparism\n");
+        for(File f:textFileList){
+        	Sorter s = new Sorter();
+        	Scanner fi = new Scanner(f);
+        	String fileName = f.getName();
+        	int dataSize = Integer.parseInt(fileName.substring(0,fileName.length()-4));
+     		int[] arr = new int[dataSize];
+     		//read in data
+     		for (int i=0;i<dataSize;i++)
+    			arr[i] = Integer.parseInt(fi.next());
 
- 		numOfKeyComparisms = s.getInsertionSortKeyComp();
- 		resultOut.print(dataSize+",	"+sortingTime+",	"+numOfKeyComparisms+"\n");
-    	}
-    resultOut.print("MergeSort\n");
-    resultOut.print("Data Size,	CPU Time,	Number of Key Comparism\n");
-    for(File f:textFileList){
-    	Sorter s = new Sorter();
-    	Scanner fi = new Scanner(f);
-    	String fileName = f.getName();
-    	int dataSize = Integer.parseInt(fileName.substring(0,fileName.length()-4));
- 		int[] arr = new int[dataSize];
- 		//read in data
- 		for (int i=0;i<dataSize;i++)
-			arr[i] = Integer.parseInt(fi.next());
+     		startTime = System.nanoTime();
+     		s.mergeSort(arr,0,dataSize-1);
+     		sortingTime = System.nanoTime() - startTime;
+     		
+     		numOfKeyComparisms = s.getMergeSortKeyComp();
+     		resultOut.print(dataSize+",	"+sortingTime+",	"+numOfKeyComparisms+"\n");
+        	}
 
- 		startTime = System.nanoTime();
- 		s.mergeSort(arr,0,dataSize-1);
- 		sortingTime = System.nanoTime() - startTime;
- 		
- 		numOfKeyComparisms = s.getMergeSortKeyComp();
- 		resultOut.print(dataSize+",	"+sortingTime+",	"+numOfKeyComparisms+"\n");
-    	}
-    resultOut.close();
-    }
+        System.out.println("Experiment Done!");
+        resultOut.close();
+        }
 }
